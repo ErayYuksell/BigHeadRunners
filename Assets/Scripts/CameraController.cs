a10c4a9cb82b7b74929a1e827846a7115df625bb
Eray Yuksel <yukseleray203@gmail.com>
2023-11-21 15:57:31 +0300

    FinishLineAdded

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offSet;
+    void Start()
+    {
+
+    }
+
+
+    void LateUpdate() // normal updatelerden sonra calisir 
+    {
+        CameraMovement();
+    }
+    void CameraMovement()
+    {
+        if (target != null)
+        {
+            transform.position = target.position + offSet;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] int playerSpeed = 5;
     float xSpeed;
     float maxXValue = 4.28f;
+    bool isPlayerMoving = true;
     void Start()
     {
 
@@ -21,6 +22,10 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMovement()
     {
+        if (isPlayerMoving == false)
+        {
+            return;
+        }
         float touchX = 0;
         float newXValue;
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) // telefonda ekran dokunusuna gore hareket 
@@ -30,12 +35,20 @@ public class PlayerController : MonoBehaviour
         }
         else if (Input.GetMouseButton(0)) // editorde mouse sol tik basilir tutarak oynatmak icin 
         {
-            xSpeed = 500f;
+            xSpeed = 350f;
             touchX = Input.GetAxis("Mouse X");
         }
         newXValue = transform.position.x + xSpeed * touchX * Time.deltaTime;
-        newXValue = Mathf.Clamp(newXValue,-maxXValue, maxXValue); // platformun disina cikmamasi icin 
+        newXValue = Mathf.Clamp(newXValue, -maxXValue, maxXValue); // platformun disina cikmamasi icin 
         playerNewPositions = new Vector3(newXValue, transform.position.y, transform.position.z + playerSpeed * Time.deltaTime);
         transform.position = playerNewPositions;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FinishLine"))
+        {
+            isPlayerMoving = false;
+        }
     }
 }
